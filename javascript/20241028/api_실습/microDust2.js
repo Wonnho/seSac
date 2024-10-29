f2726b0c164d6471b82f2cef9e05c40706b76f6b
wonnho71 <wonnho@nate.com>
2024-10-29 13:26:01 +0900

    api params, 미세정보 카테고리 나누기

---
+++ javascript/20241028/api_실습/microDust2.js
@@ -0,0 +1,18 @@
+// 3
+
+async function fetchDustInfo() {
+  const URL =
+    'https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=%2BiEaRW7xrORUYANDjKPrnvW9DAAqDJNKv3E4sm3Vwbes8db4rFSa%2FTnEVPEmaCWv1BzeVE2ek9Fv8onYt9obpQ%3D%3D&returnType=json&numOfRows=100&pageNo=1&sidoName=%EC%84%9C%EC%9A%B8&ver=1.0';
+  const res = await fetch(URL);
+  let data = await res.json();
+
+  data = data.response.body.items;
+  const objectData={}
+   for(let datum of data) {
+    const stationName=datum.stationName;
+    objectData[stationName]=datum
+   }
+
+   console.log(objectData['관악구'])
+}
+fetchDustInfo();
---
+++ javascript/20241028/api_실습/microDust3.js
@@ -0,0 +1,13 @@
+// 3
+
+async function fetchDustInfo() {
+  const URL =
+    'https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=%2BiEaRW7xrORUYANDjKPrnvW9DAAqDJNKv3E4sm3Vwbes8db4rFSa%2FTnEVPEmaCWv1BzeVE2ek9Fv8onYt9obpQ%3D%3D&returnType=json&numOfRows=100&pageNo=1&sidoName=%EC%84%9C%EC%9A%B8&ver=1.0';
+  const res = await fetch(URL);
+  let data = await res.json();
+
+  data = data.response.body.items;
+ console.log(data)
+
+}
+fetchDustInfo();
