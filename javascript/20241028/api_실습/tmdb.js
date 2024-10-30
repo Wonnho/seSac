312d62d1dd2a51c333f2fcbf3ad10f8a6322ada0
wonnho71 <wonnho@nate.com>
2024-10-30 11:41:11 +0900

    repeat TMDB api

--- javascript/20241028/api_실습/tmdb.js
+++ javascript/20241028/api_실습/tmdb.js
@@ -1,3 +1,4 @@
+// 접속 시험
 // const url = 'https://api.themoviedb.org/3/tv/popular?api_key=3aad8aaf513a9fc4cde034d9a4944177';
 // const options = {
 //   method: 'GET',
@@ -12,14 +13,17 @@
 //   .then(json => console.log(json))
 //   .catch(err => console.error(err));
 
+//대중적인 영화 첫번째 제목 가져오기
 async function getPost() {
   try {
-    const url =
-      'https://api.themoviedb.org/3/tv/popular?api_key=3aad8aaf513a9fc4cde034d9a4944177';
-    const response = await fetch(url);
+    const baseURL = 'https://api.themoviedb.org/3/movie';
+    const path = '/popular';
+    const params = 'api_key=3aad8aaf513a9fc4cde034d9a4944177';
+    const URL = `${baseURL}${path}?${params}`;
+    const response = await fetch(URL);
     const data = await response.json();
     const movies = data.results;
-    const movie = movie[0];
+    const movie = movies[0];
     //  console.log(movies)
     console.log('movie titles:', movie.title);
   } catch (error) {
@@ -28,3 +32,43 @@ async function getPost() {
 }
 
 getPost();
+
+// maximal average rating out of movies playing now
+//plus movie name having maximal average rating
+
+async function getHighRating() {
+  const baseURL = 'https://api.themoviedb.org/3/movie';
+  const path = '/popular';
+  const params = 'api_key=3aad8aaf513a9fc4cde034d9a4944177';
+  const URL = `${baseURL}${path}?${params}`;
+
+  const response = await fetch(URL);
+  const data = await response.json();
+  const movies = data.results;
+
+  let max_rating = 0;
+  let maxRatingMovie = null;
+
+  for (let movie of movies) {
+    let rating = movie.vote_average;
+    if (max_rating < rating) {
+      max_rating = rating;
+      maxRatingMovie = movie.title;
+    }
+  }
+  console.log('highest rating of movie:', max_rating);
+  console.log('movie having maximal average rating: ', maxRatingMovie);
+}
+
+getHighRating();
+
+
+// maximal average rating out of movies playing now via reduce
+//plus movie name having maximal average rating
+
+async function getVoteAvg() {
+
+  try {
+    const baseURL='https://api.themoviedb.org/3/movie'
+  }
+}
